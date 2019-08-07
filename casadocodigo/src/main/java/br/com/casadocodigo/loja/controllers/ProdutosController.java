e68dc5243576fcdd3063d91b757d37bb3feac009
Everton Paiva <evertonpaiva@gmail.com>
2019-08-07 14:23:59 -0300

    Criando página para exibir os detalhes de um produto.

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,7 +27,7 @@ import br.com.casadocodigo.loja.validation.ProdutoValidation;
 public class ProdutosController {
 	
 	@Autowired
-	private ProdutoDAO dao;
+	private ProdutoDAO produtoDao;
 	
 	@Autowired
 	private FileSaver fileSaver;
@@ -53,16 +54,24 @@ public class ProdutosController {
 		
 		String path = fileSaver.write("arquivos-sumario", sumario);
 		produto.setSumarioPath(path);
-		dao.gravar(produto);
+		produtoDao.gravar(produto);
 		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
 		return new ModelAndView("redirect:produtos");
 	}
 	
 	@RequestMapping( method=RequestMethod.GET)
 	public ModelAndView listar() {
-		List<Produto> produtos = dao.listar();
+		List<Produto> produtos = produtoDao.listar();
 		ModelAndView modelAndView = new ModelAndView("produtos/lista");
 		modelAndView.addObject("produtos", produtos);
+		return modelAndView;
+	}
+	
+	@RequestMapping("/detalhe/{id}")
+	public ModelAndView detalhe(@PathVariable("id") Integer id) {
+		ModelAndView modelAndView = new ModelAndView("produtos/detalhe");
+		Produto produto = produtoDao.find(id);
+		modelAndView.addObject("produto", produto);
 		return modelAndView;
 	}
 }
